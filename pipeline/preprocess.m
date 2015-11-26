f7309e6853beac0787bdffb549d1d130115121dd
grih7 <w.kleszcz777@gmail.com>
2015-11-26 09:14:21 +0100

    Update preprocess.m

--- pipeline/preprocess.m
+++ pipeline/preprocess.m
@@ -1,5 +1,4 @@
 function data = preprocess(images)
-  process = im_box([],0,1)*im_rotate*im_box([],1,0)*im_resize([50 50]);
-  
+  process = im_box([],0)*im_box([],0,1)*im_resize([68 68])*im_rotate*im_box([],1,0);
   data = images*process;
 end
