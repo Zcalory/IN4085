765188c63f96ab0580860db8e589657e696d0ce6
gweishar <guilherme.w123@gmail.com>
2015-11-26 09:08:29 +0100

    feature extraction via wavelet
    might be to complex for the project, but maybe we can find something
    usefull

--- pipeline/preprocess.m
+++ pipeline/preprocess.m
@@ -1,4 +1,5 @@
 function data = preprocess(images)
-  process = im_box([],0,1)*im_rotate*im_box([],1,0);
+  process = im_box([],0,1)*im_rotate*im_box([],1,0)*im_resize([50 50]);
+  
   data = images*process;
 end
