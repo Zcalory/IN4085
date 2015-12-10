de5e6ceceeab4a4fbcc473b1fc20d22d6d4324e3
gsavaia <gianluca.savaia@gmail.com>
2015-12-10 10:26:58 +0100

    kernel script

--- pipeline/raw_kernelization.m
+++ pipeline/raw_kernelization.m
@@ -1,20 +1,27 @@
 raw = loadDataset(); %load dataset
 
 classifier = fisherc;
+kernel = proxm([], 'r', 23);
+
 training_samples = 100; %number of samples per class
 
 %generate training/test set by having balanced classes
 [train, test] = gendat(raw, training_samples*ones(1,10)); clear raw; 
 
-w = train*classifier*classc; %train the classifier
+kernel = train*kernel;
 
-classification = test*w; %test the classifier on the test set
+train_k = train*kernel;
+test_k = test*kernel;
+
+w = train_k*classifier*classc; %train the classifier
+
+classification = test_k*w; %test the classifier on the test set
 
 lab = classification*labeld; %get the 'predicted' labeling 
 
 [e,c] = testc(classification); %performance of the classifier
 
-cmat = confmat( getlab(test), lab ); %compute confusion matrix
+cmat = confmat( getlab(test_k), lab ); %compute confusion matrix
 
 %save a struct in the workspace, ready to be saved in a file
 
