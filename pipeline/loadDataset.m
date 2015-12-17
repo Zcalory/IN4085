ed509d33dbc51be18662c3ddd6c21ef4cf2eedd7
gsavaia <gianluca.savaia@gmail.com>
2015-12-17 13:47:27 +0100

    new experiments (PCA)

--- pipeline/loadDataset.m
+++ pipeline/loadDataset.m
@@ -13,5 +13,11 @@ function set = loadDataset(dataset)
     if dataset == 's'
         set = load('../dataset/smoothed.mat');
         set = set.smoothed;
-    end 
+    end
+    
+    if dataset == 'v'
+        set = load('../dataset/sifted.mat');
+        set = set.sifted;
+    end
+    
 end
--- pipeline/raw_pca.m
+++ pipeline/raw_pca.m
@@ -1,16 +1,26 @@
-function [gimme_a_name] = raw_kernelization(dataset)
+function [gimme_a_name] = raw_pca(dataset)
 
 classifier = fisherc;
-kernel_type = proxm([], 'r', 23);
+kernel_type = proxm([], 'r', 75);
+pca = scalem([],'variance')*pcam([],0.99);
 lambda = 0;
 
 training_samples = 100; %number of samples per class
 
 %generate training/test set by having balanced classes
 
-for i=1:5
-    [train, test] = gendat(dataset, training_samples*ones(1,10));
-
+for i=1:1
+    disp( ['******* ' num2str(i) ' *******'] );
+    
+    [tr, te] = gendat(dataset, training_samples*ones(1,10));
+    
+    dict = tr*pca;
+    
+    train = tr*dict;
+    test = te*dict;
+    
+    disp(dict);
+    
     kernel = train*kernel_type;
 
     train_k = train*kernel + lambda*eye(size(train,1));
@@ -22,8 +32,12 @@ for i=1:5
 
     lab = classification*labeld; %get the 'predicted' labeling 
 
-    [e(i),c] = testc(classification); %performance of the classifier
-
+    [e(i),c(i,:)] = testc(classification); %performance of the classifier
+    
+    disp(e);
+    disp(c(i,:));
+    
+    disp('***************************************');
 end
     
 cmat = confmat( getlab(test_k), lab ); %compute confusion matrix
