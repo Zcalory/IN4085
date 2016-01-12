de6b6594918ea542575afeadcfe8b57ae4a7b1fc
grih7 <w.kleszcz777@gmail.com>
2016-01-12 20:20:20 +0100

    comments added

--- pipeline/disSimPE_classification.m
+++ pipeline/disSimPE_classification.m
@@ -1,7 +1,7 @@
 function [ gimme_a_name ] = disSimPE_classification(d , size_train_vec , classifier)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
+%DISSIMPE_CLASSIFICATION Test classifiers applied to pseudo-euclidean space
 
+% Choose classifier
 switch lower(classifier)
     case 'nmcpe'
         clfr = pe_nmc;
@@ -13,22 +13,23 @@ switch lower(classifier)
         clfr = pe_libsvc;
 end
 
-
+% Iterate over size of training set
 for i = 1 : length(size_train_vec)
     [dt , ds] = getDissimData(d , size_train_vec(i) , size_train_vec(i) , 'random');
-    
+    % Apply pseud-euclidean embedding to dissimilarity matrix
     w = dt * pe_em;
     xt = dt * w;
     xs = ds * w;
-    
+    % Train and test classifier
     w = xt * clfr;
     classification = xs * w;
     lab = classification * labeld;
-
+    % Calculate error and confusion matrix
     e(i) = classification * testc;
     cmat{i} = confmat(getlab(xs), lab);
 end
 
+% Save data from a test
 gimme_a_name = struct();
 
 gimme_a_name.classifier = classifier;
--- pipeline/getDissimData.m
+++ pipeline/getDissimData.m
@@ -1,22 +1,30 @@
 function [ dt , ds ] = getDissimData( d , size_train , size_rep , method)
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+%GETDISSIMDATA Split dissimilarity matrix into training and test set
 
+% If size of training set = size of representation, just split the data
+% into training and test sets
 if size_train == size_rep
     [dt , ds] = genddat(d , size_train*ones(1,10));    
 else
     if strcmp(method , 'random')
+        % Choose random objects from each class as representation set
         [dt , ds] = genddat(d , size_train*ones(1,10) , size_rep*ones(1,10));
     elseif strcmp(method , 'forward_feat')
+        % Choose objects from each class as representation set using
+        % forward feature selection
         [dt1 , ds1] = genddat(d , size_train*ones(1,10));
         crit = 'eucl-s'; % try using other criterias
         w = dt1 * featself([] , crit , size_rep);
+        % Transform training and test sets to dissimilarity space
         dt = dt1*w;
         ds = ds1*w;
     elseif strcmp(method , 'indiv_feat')
+        % Choose objects from each class as representation set using
+        % individual feature selection
         [dt1 , ds1] = genddat(d , size_train*ones(1,10));
         crit = 'eucl-s'; % try using other criterias
         w = dt1 * featseli([] , crit , size_rep);
+        % Transform training and test sets to dissimilarity space
         dt = dt1*w;
         ds = ds1*w;
     end
